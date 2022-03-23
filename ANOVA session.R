909a7edd919b8925e312ae34cf33fd60155dd92d
Saeed Shafiei Sabet <saeedfisheries@gmail.com>
2022-03-23 21:17:13 +0430

    Start
    Start

---
+++ ANOVA session.R
@@ -0,0 +1,20 @@
+# Title: ANOVA for absolute beginners tutorial
+# Script purpose: ANOVA to investigate how frogspawn hatching time varies with temperature.
+# Author - contact details
+# Date
+
+# Sources and Licences:
+# Data created by @ericazaja and licenced under the MIT agreement
+# (https://github.com/EdDataScienceEES/tutorial-ericazaja/tree/master/LICENSE.md).
+# Icons from website phylopic (http://phylopic.org/),
+# distributed under the Creative Commons license (https://creativecommons.org/)
+# Photos from Getty Images (https://www.gettyimages.co.uk/eula)
+# made available for use (https://www.gettyimages.co.uk/eula)
+
+### WORKFLOW: main sections ----
+# 1. Setting up: working directory, loading libraries, importing data
+# 2. Data formatting: tidying and exploring data
+# 3. Data visualisation: histogram and boxplot
+# 4. One-way ANOVA: model, table, assumptions
+# 5. Communicating results: barplot
+# 6. BONUS: adding phylopic and making panel
